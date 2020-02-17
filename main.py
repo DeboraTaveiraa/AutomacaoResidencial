b3e5f651ff85bc14ccd6507bf3ac15b01411f2e0
Débora Taveira <51443685+deborataveira19@users.noreply.github.com>
2020-02-17 10:28:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 
-#from pyfirmata import Arduino, util
-#import time
+from pyfirmata import Arduino, util
+import time
 
 class PaginaInicial(ScreenManager):
     pass
@@ -15,28 +15,28 @@ class Menu(Screen):
 
 class Controles(Screen):
     status = False
-    #placa = Arduino("COM3")
+    placa = Arduino("COM3") #Porta onde a placa está conectada
 
-    #placa.digital[7].write(1)
+    placa.digital[7].write(1) #Porta 7 do arduino, 1 para ligar a lâmpada
 
-    def statusLampada(self):
+    def statusLampada(self): #Botão de acionamento da lâmpada
 
         self.status = not self.status
         self.ids.btlamp.text = 'Desligar Lampada' if self.status else 'Ligar Lampada'
 
-        '''if self.ids.btlamp.text == 'Desligar Lampada':
-            self.placa.digital[7].write(0)
+        if self.ids.btlamp.text == 'Desligar Lampada':
+            self.placa.digital[7].write(0) # 0 para desligar a lâmpada
         else:
-            self.placa.digital[7].write(1)'''
+            self.placa.digital[7].write(1)
 
-    def statusVentilador(self):
+    def statusVentilador(self): #Botão de acionamento do ventilador 
         self.status = not self.status
         self.ids.btvent.text = 'Desligar Ventilador' if self.status else 'Ligar Ventilador'
 
-       #if self.ids.btvent.text == 'Desligar Ventilador':
-            #self.placa.digital[5].write(1)
-        #else:
-            #self.placa.digital[5].write(0)
+       if self.ids.btvent.text == 'Desligar Ventilador':
+            self.placa.digital[5].write(1)
+        else:
+            self.placa.digital[5].write(0)
 
     def on_pre_enter(self):
         Window.bind(on_keyboard=self.voltar)
@@ -48,10 +48,6 @@ class Controles(Screen):
     def on_pre_leave(self):
         Window.unbind(on_keyboard=self.voltar)
 
-    def mudarTexto(self, buttonl):
-        self.buttonl.text = 'Desligar lampada'
-
-    
 
 class Automatizacao(App):
     def build(self):
